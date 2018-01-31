6cf9056efd7bea89fba884a185a9ee27d68357ea
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-01-31 01:06:14 +0100

    Parameters overriding.

--- support/parameters_support.groovy
+++ support/parameters_support.groovy
@@ -8,6 +8,8 @@ class ParameterList {
     List<Parameter> parameters
 }
 
+_definedParameters = [:]
+
 string = { config -> 
     return new Parameter(name: config.name,
                          defaultValue: config.defaultValue,
@@ -28,6 +30,11 @@ properties = { props ->
     props.each { prop ->
         if (prop instanceof ParameterList) {
             prop.parameters.each {
+                if (_definedParameters.containsKey(it.name)) {
+                    _global[it.name] = _definedParameters[it.name]
+                    return                    
+                }
+
                 _global[it.name] = it.defaultValue
             }
 
