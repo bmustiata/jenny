daecf61e6d404976bdcbcc7dafb3bc833a479450
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-02-20 15:43:43 +0100

    Moved the workspace creation into support.

---
+++ info/workspace.groovy
@@ -0,0 +1,11 @@
+/**
+ * Creates a new workspace for the execution.
+ */
+_prepareWorkspace = { ->
+    def projectFolderName = _jennyConfig.projectFolder.name
+    def workspaceLocation = new File("/tmp/.jenny/workspace/${projectFolderName}")
+
+    println(_currentIndent("> workspace: ${workspaceLocation.canonicalPath}"))
+
+    return workspaceLocation
+}
