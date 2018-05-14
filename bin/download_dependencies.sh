64df59f23efdbce8c28a8a6f5e877794ef2e1338
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-05-14 21:03:04 +0200

    (#14) readlink -f mac support. Removed maven downloading.

---
+++ bin/download_dependencies.sh
@@ -0,0 +1,17 @@
+
+set -e
+
+download_file() {
+    url_to_download="$1"
+    file_to_save="target/dependency/$2"
+
+    wget "$url_to_download" -O "$file_to_save"
+}
+
+mkdir -p target/dependency/
+
+download_file https://repo.maven.apache.org/maven2/org/yaml/snakeyaml/1.17/snakeyaml-1.17.jar snakeyaml-1.17.jar
+download_file https://repo.maven.apache.org/maven2/commons-io/commons-io/2.6/commons-io-2.6.jar commons-io-2.6.jar
+download_file https://repo.maven.apache.org/maven2/commons-cli/commons-cli/1.3.1/commons-cli-1.3.1.jar commons-cli-1.3.1.jar
+download_file https://repo.maven.apache.org/maven2/org/codehaus/groovy/groovy-all/2.4.15/groovy-all-2.4.15.jar groovy-all-2.4.15.jar
+
