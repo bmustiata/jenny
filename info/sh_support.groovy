b9bb268b5b055b0cd0b5a48cf3b545204cfd132b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-15 20:34:23 +0200

    (#48) Show command in sh blocks.

--- info/sh_support.groovy
+++ info/sh_support.groovy
@@ -1,3 +1,15 @@
-sh = { code ->
-    _log.message(_currentIndent("sh"))
+sh = { config ->
+    if (!(config instanceof Map)) {
+        config = [
+            script: config as String
+        ]
+    }
+
+    def code = config.script.stripIndent().trim().replaceAll("\n", "; ")
+
+    if (code.length() > 100) {
+        code = code.substring(0, 10) + " .. " + code.substring(code.length() - 86)
+    }
+
+    _log.message(_currentIndent("sh: ${code}"))
 }
